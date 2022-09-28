0a02e945d5c8a4e7aa3e1ad53f9da94e2cf519dc
diwaandrew <diwaandre@gmail.com>
2022-09-28 10:01:54 +0700

    initialize commit

---
+++ frontend/pages/index.js
@@ -0,0 +1,39 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import {useState, useEffect} from 'react'
+
+export default function Home() {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    getData()
+  }, []);
+
+  const getData = async () => {
+    try {
+      const res = await fetch('http://localhost:8080/');
+      const data = await res.json();
+      setData(data);
+    }
+    catch (error) {
+      setError(error);
+    }
+  }
+
+  if (error) {
+    return <div>Failed to load {error.toString()}</div>
+  }
+
+  if (!data) {
+    return <div>Loading...</div>
+  }
+
+  return (
+    <div>
+      <p>data: {data.message}</p>
+    </div>
+  )
+
+  
+}
