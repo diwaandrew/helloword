bd031c9fa6686ef412b7cc0ef0c02035a3311e05
diwaandrew <diwaandre@gmail.com>
2022-09-28 15:59:19 +0700

    push front end

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,8 +1,4 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import {useState, useEffect} from 'react'
-
 export default function Home() {
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
@@ -20,20 +16,76 @@ export default function Home() {
       setError(error);
     }
   }
-
-  if (error) {
-    return <div>Failed to load {error.toString()}</div>
-  }
-
-  if (!data) {
-    return <div>Loading...</div>
-  }
-
   return (
     <div>
-      <p>data: {data.message}</p>
+      {error && <div>Failed to load {error.toString()}</div>}
+        {
+          !data ? <div>Loading...</div>
+            : (
+              (data?.data ?? []).length === 0 && <p>data kosong</p>
+            )
+        }
+
+      <Input onSuccess={getData} />
+      {data?.data ? data.data.map((item, index) => (
+        <p key={index}>{item}</p>
+      )) :
+        <p>data kosong</p>
+      }
+    </div>
+    
+  )
+ 
+
+  // if (error) {
+  //   return <div>Failed to load {error.toString()}</div>
+  // }
+
+  // if (!data) {
+  //   return <div>Loading...</div>
+  // }
+
+  // if (!data.data) {
+  //   return <p>data kosong</p>
+  // }
+
+  // return (
+  //   <div>
+  //     {data.data.map((item, index) => (
+  //       <p key={index}>{item}</p>
+  //     ))}
+  //   </div>
+  // )
+}
+function Input({onSuccess}) {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const body = {
+      text: formData.get("data")
+    }
+
+    try {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/send`, {
+        method: 'POST',
+        body: JSON.stringify(body)
+      });
+      const data = await res.json();
+      setData(data.message);
+      onSuccess();
+    }
+    catch (error) {
+      setError(error);
+    }
+  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input name="data" type="text" />
+        <button >Submit</button>
+      </form>
     </div>
   )
-
-  
 }
