354fe7f584090d6c69ebadbef0195f41f1e9b1ef
diwaandrew <diwaandre@gmail.com>
2022-09-29 11:05:52 +0700

    feat: added database support

--- backend/main.go
+++ backend/main.go
@@ -7,40 +7,104 @@ import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	_ "github.com/joho/godotenv/autoload"
+
+	"github.com/glebarez/sqlite"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
-func handler(c *gin.Context) {
+type repository struct {
+	db *gorm.DB
+}
+
+func (r *repository) handler(c *gin.Context) {
+	var todos []Todos
+	res := r.db.Find(&todos)
+	if res.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": res.Error.Error()})
+		return
+	}
+
 	c.JSON(http.StatusOK, gin.H{
-		"data": db,
+		"data": todos,
 	})
 }
 
-var db []string
-
-type DataRequest struct {
-	Text string `json:"text"`
-}
-
-func postHandler(c *gin.Context) {
+func (r *repository) postHandler(c *gin.Context) {
 	var data DataRequest
 	if err := c.ShouldBindJSON(&data); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	db = append(db, data.Text)
-	c.JSON(http.StatusOK, gin.H{"message": "data berhasil terkirim", "data": data.Text})
+
+	todo := Todos{
+		Task: data.Task,
+		Done: false,
+	}
+
+	res := r.db.Create(&todo)
+	if res.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": res.Error.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "data berhasil terkirim", "data": todo})
+}
+
+type Todos struct {
+	gorm.Model
+	Task string `json:"task"`
+	Done bool   `json:"done"`
+}
+
+type DataRequest struct {
+	Task string `json:"task" binding:"required"`
 }
 
 func main() {
 
-	db = make([]string, 0)
-	r := gin.Default()
+	// db, err := gorm.Open(sqlite.Open("database.db"), &gorm.Config{})
+	// if err != nil {
+	// 	panic("failed to connect database")
+	// }
 
+	var db *gorm.DB
+	var err error
+	dbUrl := os.Getenv("DATABASE_URL")
+
+	if os.Getenv("ENVIRONMENT") == "PROD" {
+		db, err = gorm.Open(postgres.Open(dbUrl), &gorm.Config{})
+	} else {
+		db, err = gorm.Open(sqlite.Open(dbUrl), &gorm.Config{})
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		panic("failed to get database")
+	}
+
+	if err := sqlDB.Ping(); err != nil {
+		panic("failed to ping database")
+	}
+
+	if err := db.AutoMigrate(&Todos{}); err != nil {
+		panic("failed to migrate database")
+	}
+
+	if err := db.AutoMigrate(&Todos{}); err != nil {
+		panic("failed to migrate database")
+	}
+
+	// db = make([]string, 0)
+	repo := repository{
+		db: db,
+	}
+	r := gin.Default()
 	r.Use(cors.New(cors.Config{
 		AllowOrigins: []string{"*"},
 	}))
 
-	r.GET("/", handler)
-	r.POST("/send", postHandler)
+	r.GET("/", repo.handler)
+	r.POST("/send", repo.postHandler)
 	r.Run(":" + os.Getenv("PORT"))
 }
